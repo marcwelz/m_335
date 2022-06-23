5ffe063db07ca22f0ddd2afe23b0cc8805ae8839
Milchmaa <marc.welz03@gmail.com>
2022-06-23 13:06:36 +0200

    init files

---
+++ 01_hello_world/App.js
@@ -0,0 +1,21 @@
+import AddTask from './components/AddTask';
+import HomeScreen from './components/HomeScreen';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: true,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add Task" component={AddTask} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
